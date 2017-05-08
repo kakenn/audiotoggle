4647fcaee2790aeeae556f9a5b03ae758f199954
三上航人 <info@shinuchi.com>
2017-05-08 17:17:08 +0900

    Add BT audio setting

--- src/android/com/dooble/audiotoggle/AudioTogglePlugin.java
+++ src/android/com/dooble/audiotoggle/AudioTogglePlugin.java
@@ -10,46 +10,57 @@ import android.media.AudioManager;
 
 public class AudioTogglePlugin extends CordovaPlugin {
 	public static final String ACTION_SET_AUDIO_MODE = "setAudioMode";
-	
+
 	@Override
-	public boolean execute(String action, JSONArray args, 
-			CallbackContext callbackContext) throws JSONException {	
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
 		if (action.equals(ACTION_SET_AUDIO_MODE)) {
 			if (!setAudioMode(args.getString(0))) {
 				callbackContext.error("Invalid audio mode");
 				return false;
 			}
-			
+
 			return true;
 		}
-		
+
 		callbackContext.error("Invalid action");
 		return false;
 	}
-	
+
 	public boolean setAudioMode(String mode) {
 	    Context context = webView.getContext();
-	    AudioManager audioManager = 
+	    AudioManager audioManager =
 	    	(AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-	    
+
 	    if (mode.equals("earpiece")) {
 	    	audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
 	    	audioManager.setSpeakerphoneOn(false);
 	        return true;
-	    } else if (mode.equals("speaker")) {        
+	    } else if (mode.equals("speaker")) {
+				audioManager.stopBluetoothSco();
+				audioManager.setBluetoothScoOn(false);
 	    	audioManager.setMode(AudioManager.STREAM_MUSIC);
 	    	audioManager.setSpeakerphoneOn(true);
 	        return true;
-	    } else if (mode.equals("ringtone")) {        
+	    } else if (mode.equals("ringtone")) {
+				audioManager.stopBluetoothSco();
+				audioManager.setBluetoothScoOn(false);
 	    	audioManager.setMode(AudioManager.MODE_RINGTONE);
 	    	audioManager.setSpeakerphoneOn(false);
-	        return true; 
-	    } else if (mode.equals("normal")) {        
-	    	audioManager.setMode(AudioManager.MODE_NORMAL);
-	    	audioManager.setSpeakerphoneOn(false);
 	        return true;
-	    }
-	    
+	    } else if (mode.equals("normal")) {
+				audioManager.stopBluetoothSco();
+				audioManager.setBluetoothScoOn(false);
+	    	audioManager.setMode(AudioManager.MODE_NORMAL);
+	        return true;
+			} else if (mode.equals("bt")) {
+				audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+				audioManager.setBluetoothScoOn(true);
+				audioManager.setSpeakerphoneOn(false);
+				audioManager.startBluetoothSco();
+					return true;
+			}
+
 	    return false;
 	}
 
